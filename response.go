fdafc31c800fdfc983a1f557be0f14b2257a733f
CrushedPixel <crushedpixelmaps@gmail.com>
2018-01-04 15:53:55 +0100

    Add SendFile function returning a response sending a file to the user

--- response.go
+++ response.go
@@ -3,64 +3,96 @@ package margo
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
+	"io"
+	"strconv"
 )
+
+const contentLengthHeader  = "Content-Length"
 
 // base Response interface
 type Response interface {
 	Send(context *gin.Context) error
 }
 
-type DoNothingResponse struct{}
+type doNothingResponse struct{}
 
-func (r *DoNothingResponse) Send(c *gin.Context) error {
+func (r *doNothingResponse) Send(c *gin.Context) error {
 	return nil
 }
 
 // Generic JSON Response
-type JSONResponse struct {
+type jsonResponse struct {
 	Status int
 	Data   interface{}
 }
 
-func (r *JSONResponse) Send(c *gin.Context) error {
+func (r *jsonResponse) Send(c *gin.Context) error {
 	c.JSON(r.Status, r.Data)
 	return nil
 }
 
 // Empty Response only setting a status code
-type EmptyResponse struct {
+type emptyResponse struct {
 	Status int
 }
 
-func (r *EmptyResponse) Send(c *gin.Context) error {
+func (r *emptyResponse) Send(c *gin.Context) error {
 	c.Status(r.Status)
 	c.Writer.WriteHeaderNow()
 	return nil
 }
 
+// Response sending a file
+type fileResponse struct {
+	file *os.File
+}
+
+func (r *fileResponse) Send(c *gin.Context) error {
+	stat, err := r.file.Stat()
+	if err != nil {
+		return err
+	}
+
+	c.Status(http.StatusOK)
+	c.Header(contentLengthHeader, strconv.FormatInt(stat.Size(), 10))
+
+	// ignore any errors writing the file,
+	// as they are most likely caused by
+	// the client closing the connection
+	io.Copy(c.Writer, r.file)
+	return nil
+}
+
 // Utility methods to create responses
-func NewJSONResponse(status int, data interface{}) *JSONResponse {
-	return &JSONResponse{
+func JSON(status int, data interface{}) *jsonResponse {
+	return &jsonResponse{
 		status, data,
 	}
 }
 
-func NewEmptyResponse(status int) *EmptyResponse {
-	return &EmptyResponse{status}
+func JSON200(data interface{}) Response {
+	return JSON(http.StatusOK, data)
 }
 
-func NewErrorResponse(status int, errors ...*MargoError) *JSONResponse {
-	return NewJSONResponse(status, gin.H{"errors": errors})
+func NewEmptyResponse(status int) Response {
+	return &emptyResponse{status}
 }
 
-func DoNothing() *DoNothingResponse {
-	return &DoNothingResponse{}
+func NewErrorResponse(status int, errors ...*MargoError) Response {
+	return JSON(status, gin.H{"errors": errors})
 }
 
-func BadRequest(error ...*MargoError) *JSONResponse {
+func DoNothing() Response {
+	return &doNothingResponse{}
+}
+
+func BadRequest(error ...*MargoError) Response {
 	return NewErrorResponse(http.StatusBadRequest, error...)
 }
 
-func OK(data interface{}) *JSONResponse {
-	return NewJSONResponse(http.StatusOK, data)
+func SendFile(file *os.File) Response {
+	return &fileResponse{
+		file: file,
+	}
 }
